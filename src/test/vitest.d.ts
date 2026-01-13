61e378eefaae4c72dd1260a6ee73c1f7edc821b8
julian pickard <jpickard.mail@gmail.com>
2026-01-13 00:04:32 +0000

    Added tests

---
+++ src/test/vitest.d.ts
@@ -0,0 +1,8 @@
+import type { TestingLibraryMatchers } from "@testing-library/jest-dom/matchers";
+import "vitest";
+
+declare module "vitest" {
+  interface Assertion<T = any> extends TestingLibraryMatchers<T, void> {}
+  interface AsymmetricMatchersContaining
+    extends TestingLibraryMatchers<any, void> {}
+}
